e6797139e7f51ee3afec7ad9d7d6ecdbdf7e28da
mahmoudRiad22 <mahmoudriad027@gmail.com>
2024-04-03 03:51:35 +0200

    pycodestyle errors

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from __future__ import print_function
 import sys
+
+
 def safe_function(fct, *args):
     try:
         res = fct(*args)
@@ -8,4 +10,4 @@ def safe_function(fct, *args):
         print("Exception: {}".format(e), file=sys.stderr)
         return None
     else:
-        return res
+        return rei
